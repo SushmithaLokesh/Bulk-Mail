5c64e82f80b953c2289a5665712b967d4fbcbb4e
SushmithaLokesh <163681850+SushmithaLokesh@users.noreply.github.com>
2024-06-22 16:20:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ function App()
   function send()
   {
     setstatus(true)
-    axios.post("http://localhost:5000/sendemail",{msg:msg,emailList:emailList})
+    axios.post("https://bulk-mail-f2ut.onrender.com/sendemail",{msg:msg,emailList:emailList})
     .then(function(data)
   {
     if(data.data == true)
